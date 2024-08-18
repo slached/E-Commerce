8ea3433f2559f028c663fece88b61fb64688abea
slached <omerbsahin@gmail.com>
2024-08-18 22:54:14 +0300

    redis implemented

--- server/MiddleWares/getOrSetCache.js
+++ server/MiddleWares/getOrSetCache.js
@@ -3,8 +3,11 @@ const {redisClient, DEFAULT_EXPIRATION} = require('../config/redis')
 const getOrSetCache = (key, cb) => {
     return new Promise((resolve, reject) => {
         redisClient.get(key, async (err, data) => {
+            // if there is an error occured
             if (err) return reject(err);
+            // if key in cache
             if (data) return resolve(JSON.parse(data))
+            // if key is not in cache
             const freshData = await cb()
             redisClient.setex(key, DEFAULT_EXPIRATION, JSON.stringify(freshData))
             resolve(freshData)
